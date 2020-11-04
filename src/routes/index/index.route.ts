e9da72b1d50ddb3052bd946483bb006c9ca45f59
Ivan Nieto <ivan@bambu-mobile.com>
2020-11-04 00:50:43 -0600

    feat: jwt authentication middleware implemented

--- src/routes/index/index.route.ts
+++ src/routes/index/index.route.ts
@@ -1,4 +1,5 @@
-import { Router } from "express";
+import {  Request, Response, NextFunction, Router } from "express";
+import jwt from "express-jwt";
 import { indexController } from "./index.controller";
 
 class IndexRoute {
@@ -9,6 +10,13 @@ class IndexRoute {
   }
 
   routes(): void {
+    this.router.use("/", jwt({ secret: process.env.JWT_SECRET as string, algorithms: ["HS256"] }), 
+      (err: Error, req: Request, res: Response, next: NextFunction) => {
+        if (err.name === "UnauthorizedError") {
+          return res.status(401).json({ error: "Unauthorized"});
+        }
+        next();
+      });
     this.router.route("/").get(indexController.getIndex);
   }
 }
